7be59621cd01faa2df4b9d25cf398c83794e4276
Bryan Malyn <bim9262@yahoo.com>
2012-03-30 15:30:32 -0400

    Start telent on launch

--- src/synthseq/Main.java
+++ src/synthseq/Main.java
@@ -5,12 +5,12 @@ import synthseq.clojureinterop.ClojureServer;
 import synthseq.oscinterop.DefaultActionMap;
 import synthseq.oscinterop.OSCServer;
 import telnet.Telnet;
-import synthseq.signalanalysis.*;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-
+		
 		ClojureServer.start(9000);
 		OSCServer.start(new DefaultActionMap(), 8000);
+		new Telnet();
 	}
 }

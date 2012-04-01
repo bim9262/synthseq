7da76b37473ce37ef70c783e23da21eac066411b
Bryan Malyn <bim9262@yahoo.com>
2012-04-01 01:51:35 -0400

    Wrote ADSR needs to be checked.

--- src/synthseq/playables/readables/envelopes/ADSR.java
+++ src/synthseq/playables/readables/envelopes/ADSR.java
@@ -3,6 +3,15 @@ package synthseq.playables.readables.envelopes;
 import synthseq.playables.readables.ReadableSound;
 
 public class ADSR extends ReadableSound {
+	double attack;
+	double peek;
+	double decay;
+	double level;
+	double release;
+	private boolean running = false;
+	private int amplitude;
+	private double time;
+
 	/*
 	 * See http://en.wikipedia.org/wiki/Synthesizer#ADSR_envelope This is
 	 * arguably one of the most important things that has to happen for the
@@ -17,25 +26,43 @@ public class ADSR extends ReadableSound {
 	 */
 	public ADSR(double attack, double peek, double decay, double level,
 			double release) {
+		this.attack = attack;
+		this.peek = peek;
+		this.decay = decay;
+		this.level = level;
+		this.release = release;
 
 	}
 
 	@Override
 	public void start() {
-		// TODO Auto-generated method stub
-
+		amplitude = 0;
+		time = 0;
+		running = true;
 	}
 
 	@Override
 	public void stop() {
-		// TODO Auto-generated method stub
-
+		running = false;
 	}
 
 	@Override
 	public double read() {
-		// TODO Auto-generated method stub
-		return 0;
+		if (!running) {
+			return 0;
+		} else {
+			time += 1 / 44100;
+		}
+		if (time >= attack) {
+			amplitude += peek / (attack * 44100);
+		} else if (time < attack && time >= attack + decay) {
+			amplitude += (level - peek) / (decay * 44100);
+		} else if (time < attack + decay && time >= attack + decay + release) {
+			amplitude += -level / (release * 44100);
+		} else {
+			running = false;
+		}
+		return amplitude;
 	}
 
 }

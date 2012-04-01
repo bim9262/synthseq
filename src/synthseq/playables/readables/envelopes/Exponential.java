2d6923127a56f37f36bc2fd7a021fdebbe2473cc
Bryan Malyn <bim9262@yahoo.com>
2012-03-31 22:30:59 -0400

    Copied implementation of linear to exp but need to make equation to change the amplitude.

--- src/synthseq/playables/readables/envelopes/Exponential.java
+++ src/synthseq/playables/readables/envelopes/Exponential.java
@@ -3,30 +3,38 @@ package synthseq.playables.readables.envelopes;
 import synthseq.playables.readables.ReadableSound;
 
 public class Exponential extends ReadableSound{
+	private double amplitude;
+	private double period;
+	private boolean running = false;
 	
 	/*
 	 * See the linear filter.
 	 * Same idea, except decay is exponential.
 	 */
 	public Exponential(double period){
-		
+		this.period = period;
 	}
+
 	@Override
 	public void start() {
-		// TODO Auto-generated method stub
-		
+		amplitude = 1;
+		running = true;
 	}
 
 	@Override
 	public void stop() {
-		// TODO Auto-generated method stub
-		
+		running = false;
 	}
 
 	@Override
 	public double read() {
-		// TODO Auto-generated method stub
-		return 0;
+		if (!running) {
+			return -1;
+		} else if (amplitude <= 0) {
+			running = false;
+		} else
+			amplitude = 0;//TODO:Change to some equation
+		return amplitude;
 	}
 
 }

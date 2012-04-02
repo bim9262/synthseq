54df52ffcdd879cd3d8a71ff5455dcc1beea193b
Bryan Malyn <bim9262@yahoo.com>
2012-04-02 00:15:24 -0400

    Prevent blanks from being saved in used commands list

--- src/telnet/CommandList.java
+++ src/telnet/CommandList.java
@@ -2,27 +2,28 @@ package telnet;
 
 import java.util.ArrayList;
 
-public class CommandList{
+public class CommandList {
 	private ArrayList<String> cmds = new ArrayList<String>();
 	public int pos;
-	
-	public String next(){
-		pos+=pos==cmds.size()?0:1;
-		return pos<cmds.size()?cmds.get(pos):"";
+
+	public String next() {
+		pos += pos == cmds.size() ? 0 : 1;
+		return pos < cmds.size() ? cmds.get(pos) : "";
 	}
-	
-	public String prev(){
-		pos-=pos==0?0:1;
-		return pos>=0?cmds.get(pos):"";
+
+	public String prev() {
+		pos -= pos == 0 ? 0 : 1;
+		return pos >= 0 ? cmds.get(pos) : "";
 	}
-	
-	public void add(String cmd){
-		if (cmds.size()==0 || !cmds.get(cmds.size()-1).equals(cmd)){
+
+	public void add(String cmd) {
+		if (cmd != null && !cmd.equals("")
+				&& (cmds.size() == 0 || !cmds.get(cmds.size() - 1).equals(cmd))) {
 			cmds.add(cmd);
 		}
-		
+
 		pos = cmds.size();
-				
+
 	}
-	
+
 }

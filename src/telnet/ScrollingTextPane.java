ca1cbe435b39e6514e7830439929b29c32e3a4e1
Bryan Malyn <bim9262@yahoo.com>
2012-04-04 12:47:39 -0400

    Changed code indentation

--- src/telnet/ScrollingTextPane.java
+++ src/telnet/ScrollingTextPane.java
@@ -7,36 +7,35 @@ import javax.swing.JTextPane;
 
 @SuppressWarnings("serial")
 public class ScrollingTextPane extends JScrollPane {
-	
+
 	private TextPane textPane;
-	
+
 	public ScrollingTextPane() {
 		super();
 		textPane = new TextPane();
 		setViewportView(textPane);
 	}
-	
-	public TextPane getTextPane(){
+
+	public TextPane getTextPane() {
 		return textPane;
 	}
-	
-	public class TextPane extends JTextPane{	
-	
-	public TextPane(){
-		super();		
-		setBackground(Color.BLACK);
-		setForeground(Color.GREEN);
-		setCaretColor(Color.RED);
-	}
-	
-	public synchronized void append (String s) {
-		setText(getText() + s
-				+ "\n");		
-	}
-	
-	public synchronized void setText(String s){
-		super.setText(s);
-		setCaretPosition(getDocument().getLength());
-	}
+
+	public class TextPane extends JTextPane {
+
+		public TextPane() {
+			super();
+			setBackground(Color.BLACK);
+			setForeground(Color.GREEN);
+			setCaretColor(Color.RED);
+		}
+
+		public synchronized void append(String s) {
+			setText(getText() + s + "\n");
+		}
+
+		public synchronized void setText(String s) {
+			super.setText(s);
+			setCaretPosition(getDocument().getLength());
+		}
 	}
 }

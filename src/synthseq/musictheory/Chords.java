1fe6325965e0cf0ca7d667a536574c5aa259d496
john <john@mooring.com>
2012-04-18 18:29:52 -0400

    Chord/note parsing, freq to midi, more conversions

--- src/synthseq/musictheory/Chords.java
+++ src/synthseq/musictheory/Chords.java
@@ -1,9 +1,6 @@
 package synthseq.musictheory;
 
-import static synthseq.musictheory.Scales.getScale;
-
 import java.util.Collection;
-
 import java.util.HashMap;
 
 public class Chords {
@@ -49,26 +46,26 @@ public class Chords {
 		chordMap.put("sus2", suspended2);
 		chordMap.put("sus4", suspended4);
 		chordMap.put("-5", flatFifth);
-		chordMap.put("6", sixth);
+		chordMap.put("6th", sixth);
 		chordMap.put("m6", minorSixth);
-		chordMap.put("7", seventh);
+		chordMap.put("7th", seventh);
 		chordMap.put("m7", minorSeventh);
 		chordMap.put("*7", diminishedSeventh);
 		chordMap.put("M7", majorSeventh);
 		chordMap.put("mM7", minorMajorSeventh);
 		chordMap.put("7/6", sevenSix);
-		chordMap.put("9",ninth);
+		chordMap.put("9th",ninth);
 		chordMap.put("m9",minorNinth);
 		chordMap.put("-9",flatNinth);
 		chordMap.put("m-9",minorFlatNinth);
 		chordMap.put("9+",augmentedNinth);
 		chordMap.put("9/6",nineSix);
 		chordMap.put("m9/6",minorNineSix);
-		chordMap.put("11",eleventh);
+		chordMap.put("11th",eleventh);
 		chordMap.put("m11",minorEleventh);
 		chordMap.put("11+",augmentedEleventh);
 		chordMap.put("m11+",minorAugmentedEleventh);
-		chordMap.put("13",thirteenth);
+		chordMap.put("13th",thirteenth);
 		chordMap.put("m13",minorThirteenth);
 		chordMap.put("13+11",thirteenthAugmentedEleventh);
 		chordMap.put("m13+11",minorThirteenthAugmentedEleventh);
@@ -76,6 +73,16 @@ public class Chords {
 
 	private int[] getRomanNumeral(int baseNote, int romanNumeral, boolean major) {
 		return getRomanNumeral(baseNote, romanNumeral, major, 3);
+	}
+	
+	public static int[] getChord(int baseNote, String chord){
+			byte[] chordNotes = chordMap.get(chord);
+			int[] notes = new int[chordNotes.length+1];
+			notes[0] = baseNote;
+			for(int i = 1; i <= chordNotes.length; i++){
+				notes[i] = notes[i-1] + chordNotes[(i-1)%chordNotes.length];
+			}
+			return notes;
 	}
 
 	private int[] getRomanNumeral(int baseNote, int romanNumeral,
@@ -89,26 +96,6 @@ public class Chords {
 
 	public static Collection<String> getChords() {
 		return chordMap.keySet();
-	}
-
-	public static void main(String[] args) {
-		boolean major = true;
-		int baseNote = 0;
-		int length = 5;
-		System.out.print("M Scale: ");
-		for (int i : getScale("major", baseNote, 2 * length - 1)) {
-			System.out.print(i + ", ");
-		}
-		System.out.print("\nm Scale: ");
-		for (int i : getScale("minor", baseNote, 2 * length - 1)) {
-			System.out.print(i + ", ");
-		}
-
-		System.out.print("\n  Chord: ");
-		for (int i : new Chords().getRomanNumeral(baseNote, 3, major, 4)) {
-			System.out.print(i + ",    ");
-		}
-
 	}
 
 }
--- src/synthseq/musictheory/Rules.java
+++ src/synthseq/musictheory/Rules.java
@@ -1,43 +1,69 @@
 package synthseq.musictheory;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Scanner;
 
 public class Rules {
 
-	private static byte[] noteSeq = { 0, 2, 3, 5, 7, 8, 10, 12 };
+	private static byte[] noteSeq = { 9, 11, 0, 2, 4, 5, 7};
+	private static String[] invSeq = {"C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B"};
 
-	public static Collection<Double> stringToFreqs(String string) {
-		ArrayList<Double> v = new ArrayList<Double>();
+	public static ArrayList<ArrayList<Double>> stringToFreqs(String string) {
+		ArrayList<ArrayList<Double>> v = new ArrayList<ArrayList<Double>>();
 		Scanner scn = new Scanner(string);
 		while (scn.hasNext()) {
-			int i = parseNote(scn.next());
-			if (i != -1)
-				v.add(noteToFreq(i));
+			ArrayList<Double> notes = new ArrayList<Double>();
+			String str = scn.next();
+			boolean contained = false;
+			for (String s : Chords.getChords()) {
+				if (str.contains(s)) {
+					contained = true;
+					break;
+				}
+			}
+			if (!contained) {
+				int i;
+				if ((i = stringToMidi(str)) != -1)
+					notes.add(midiToFreq(i));
+			} else {
+				String chord = str.replaceAll("[a-gA-G][s#b]?-?[0-9]?", "");
+				String baseNote = str.replace(chord, "");
+				System.out.println(baseNote+" - "+chord);
+				for(int i : Chords.getChord(stringToMidi(baseNote),chord))
+					notes.add(midiToFreq(i));
+			}
+			v.add(notes);
 		}
 		return v;
 	}
 
-	public static int parseNote(String str) {
+	public static String midiToString(int i) {
+		return invSeq[Math.abs(i%12)] + (i / 12 - 1);
+	}
+	
+	public static int freqToMidi(double f){
+		return (int) (12*(Math.log(f/440)/Math.log(2))+69);
+	};
+
+	public static int stringToMidi(String str) {
 		if (!str.matches("[a-gA-G][s#b]?-?[0-9]?"))
 			return -1;
-		int baseNote = noteSeq[(Character.toUpperCase(str.charAt(0)) - 65)] + 69;
+		int baseNote = noteSeq[(Character.toUpperCase(str.charAt(0)) - 'A')] + 60;
 		str = str.substring(1);
 		if (str.matches("[s#b]-?[0-9]?")) {
 			if (str.charAt(0) == 's' || str.charAt(0) == '#')
 				baseNote++;
 			else if (str.charAt(0) == 'b')
 				baseNote--;
-			str = str.substring(1);
+			str = str.replaceAll("[s#b]","");
 		}
-		if (str.matches("[0-9]"))
-			baseNote += 12 * (Integer.parseInt(str) - 4);
+		if (str.matches("-?[0-9]"))
+			baseNote += 12 * (Integer.parseInt(str) - (baseNote>7?4:3));
 		return baseNote;
 	}
 
-	public static double noteToFreq(int note) {
-		return  440 * Math.pow(2, (note - 69) / 12.0);
+	public static double midiToFreq(int note) {
+		return 440 * Math.pow(2, (note - 69) / 12.0);
 	}
 
 }
